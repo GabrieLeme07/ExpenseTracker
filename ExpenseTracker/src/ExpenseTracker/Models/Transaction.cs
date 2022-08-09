fedcef689e0375263a29bcfabd63458b29d86fdb
Gabriel Leme <gabriel.leme.1@hotmail.com>
2022-08-08 21:57:08 -0300

    Implementation

---
+++ ExpenseTracker/src/ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models;
+public class Transaction
+{
+    [Key]
+    public Guid TransactionId { get; set; }
+
+    //EF
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public int Amount { get; set; }
+
+    [Column(TypeName = "nvarchar(50)")]
+    public string? Note { get; set; }
+
+    public DateTime Date { get; set; } = DateTime.Now;
+}
+
